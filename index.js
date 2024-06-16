85850a9324a613cfe864c3d3683b7fdbbe5a935f
Yusuf Kahya <niqhtswordxd.yk@gmail.com>
2024-06-16 21:26:08 +0000

    chore: Update .gitignore and package.json

--- index.js
+++ index.js
@@ -1,11 +1,89 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Url = require("./models/urlModel");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Function to connect to the MongoDB database
+const connectDB = async () => {
+  try {
+    // mongodb connection string
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongoDB connected.");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+// connect to mongoDB
+connectDB();
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Middleware for handling JSON, URL-encoded data, and serving static files
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+// start the server and listen on PORT
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`);
+});
+
+app.post("/shorten", async (req, res) => {
+  try {
+    const url = new Url({ fullUrl: req.body.fullUrl , name: req.body.name});
+    await url.save();
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Invalid URL");
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const urls = await Url.find();
+    res.render("index", { urls });
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+
+app.get('/:shortUrl', async (req, res) => {
+  try {
+    const shortUrl = req.params.shortUrl;
+    const url = await Url.findOne({ shortUrl });
+    if (!url) {
+      return res.status(400).send('URL not found');
+    }
+ // Increment the click count and save the updated URL
+    url.clicks++;
+    url.save();
+    res.redirect(url.fullUrl);
+  } catch (error) {
+    res.status(500).send('URL not found');
+  }
+});
+
+app.post("/update/:shortUrl", async (req, res) => {
+  try {
+    const shortUrl = req.params.shortUrl;
+    const newFullUrl = req.body.newFullUrl;
+    const url = await Url.findOne({ shortUrl });
+
+    if (!url) {
+      return res.status(400).send('URL not found');
+    }
+
+    url.fullUrl = newFullUrl;
+    await url.save();
+    res.send('URL updated successfully');
+  } catch (error) {
+    res.status(500).send('Error updating URL');
+  }
+});
